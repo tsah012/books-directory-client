34ddf1c2bf5d5125913f957be798a10a96312e03
tsah012 <tsah012@gmail.com>
2021-08-10 01:15:43 +0300

    Add app bar component
    Signed-off-by: tsah012 <tsah012@gmail.com>

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Login from '../Login'
+import AppBar from '../AppBar'
 
 function App() {
     return (
         <>
             <div>
                 <h1>My App</h1>
+                <AppBar />
                 <Login />
             </div>
             
---
+++ src/Components/AppBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+function AppBar() {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Contact</a></li>
+                <li style={{float:'right'}}><a className="active" href="#">Logout</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default AppBar
